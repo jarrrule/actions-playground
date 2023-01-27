292067249e2f580fb10607737c59322d4f90dfb8
sean darley <sean.darley@sainsburys.co.uk>
2023-01-27 16:32:24 +0000

    no message

--- .github/actions/deploy/src/index.js
+++ .github/actions/deploy/src/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-const brand = brand.getInput('brand')
+const brand = core.getInput('brand')
 console.log(`what is the brand ${brand}`)
 core.setOutput('deploy-suffix', `deploy-suffix from action ${brand}`)
