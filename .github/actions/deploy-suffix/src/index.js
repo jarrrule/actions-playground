77006b202b99fa30d0b7fbf4b9a5ed080a608dd3
sean darley <sean.darley@sainsburys.co.uk>
2023-01-30 09:57:27 +0000

    no message

--- .github/actions/deploy-suffix/src/index.js
+++ .github/actions/deploy-suffix/src/index.js
@@ -1,11 +1,9 @@
 const core = require('@actions/core')
 
-const deploySuffix = core.getInput('deploy-suffix')
-const placeholder = core.getInput('placeholder')
-const brand = core.getInput('brand')
-
-if (placeholder) {
-  core.setOutput('value',  deploySuffix.replace(placeholder, brand))
-} else {
-  core.setOutput('value', deploySuffix)
+try {
+  const deploySuffix = core.getInput('deploy-suffix')
+  const brand = core.getInput('brand')
+  core.setOutput('value', deploySuffix.replace('*****', brand))
+} catch (error) {
+  core.setFailed(error.message)
 }
