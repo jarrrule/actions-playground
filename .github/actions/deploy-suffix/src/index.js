374761b8008decd896b8c184e05c7d47028ebd85
jarrrule <jarrrule@users.noreply.github.com>
2023-01-31 15:08:07 +0000

    Automated deployment: Tue Jan 31 15:08:07 UTC 2023 00899395b5be384cea9198827c1fd50655a93607

--- .github/actions/deploy-suffix/src/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core')
-
-try {
-  const deploySuffix = core.getInput('deploy-suffix')
-  const brand = core.getInput('brand')
-  core.setOutput('value', deploySuffix.replace('*****', brand))
-} catch (error) {
-  core.setFailed(error.message)
-}
