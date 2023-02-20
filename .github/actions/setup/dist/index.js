d9f971789e925ab2bdf3e67f56dabddf53a76b4b
sean darley <sean.darley@sainsburys.co.uk>
2023-02-20 09:26:46 +0000

    no message

--- .github/actions/setup/dist/index.js
+++ .github/actions/setup/dist/index.js
@@ -801,6 +801,7 @@ try {
   const deployTo = core.getInput('deploy-to')
   console.log('inside action ', deployTo)
   core.setOutput('node-env', 'sean darley')
+  core.setOutput('clean-url', 'url that will be cleaned')
 } catch (error) {
   core.setFailed(error.message)
 }
