decb6c28a30c003515b4ca4a050066cae4c4f8e6
sean darley <sean.darley@sainsburys.co.uk>
2023-02-21 07:49:56 +0000

    no message

--- .github/actions/setup/dist/index.js
+++ .github/actions/setup/dist/index.js
@@ -2487,14 +2487,14 @@ const getNodeEnv = (env) => {
 
 const getCleanDeploySuffix = (deploySuffix) => {
   if (!deploySuffix) return ''
-  return deploySuffix.replaceAll('/', '-').toLowerCase()
+  return `-${deploySuffix.replaceAll('/', '-').toLowerCase()}`
 }
 
 try {
   const env = core.getInput('env')
   const deploySuffix = core.getInput('deploy-suffix')
   core.setOutput('node-env', getNodeEnv(env))
-  core.setOutput('brand-deploy-suffix', `-${getCleanDeploySuffix(deploySuffix)}`)
+  core.setOutput('brand-deploy-suffix', `${getCleanDeploySuffix(deploySuffix)}`)
 } catch (error) {
   core.setFailed(error.message)
 }
