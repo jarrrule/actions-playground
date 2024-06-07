93f09951641ed00be98c52dd59d5c73168ed56ee
jarrrule <sean.darley@sainsburys.co.uk>
2024-06-07 15:18:27 +0100

    FDD-3422: blar balr

--- .github/actions/getIssuesInRelease/index.js
+++ .github/actions/getIssuesInRelease/index.js
@@ -2,17 +2,6 @@ import core from '@actions/core'
 import github from '@actions/github'
 import { Octokit } from "@octokit/core"
 
-try {
-  const prNumber = core.getInput('pr-number');
-  console.log(`Hello ${prNumber}!`);
-  core.setOutput('jira-issues', ['NAP-1234', 'NAP-1235', 'NAP-1236', 'NAP-1237'])
-  const token = core.getInput('token')
-  console.log('can you see the token ', token)
-  console.log(JSON.stringify(makeRequest(), null, 2))
-} catch (error) {
-  core.setFailed(error.message)
-}
-
 const octokit = new Octokit({
   auth: core.getInput('token')
 })
@@ -27,5 +16,18 @@ const makeRequest = async () => {
     }
   })
 }
+
+try {
+  const prNumber = core.getInput('pr-number');
+  console.log(`Hello ${prNumber}!`);
+  core.setOutput('jira-issues', ['NAP-1234', 'NAP-1235', 'NAP-1236', 'NAP-1237'])
+  const token = core.getInput('token')
+  console.log('can you see the token ', token)
+  console.log(JSON.stringify(await makeRequest(), null, 2))
+} catch (error) {
+  core.setFailed(error.message)
+}
+
+
 
 
