6992015110433fb263cb4ef3bef927a7b7216bae
jarrrule <sean.darley@sainsburys.co.uk>
2024-06-07 15:41:57 +0100

    TTT-6754f: ytes

--- .github/actions/getIssuesInRelease/dist/index.js
+++ .github/actions/getIssuesInRelease/dist/index.js
@@ -33302,9 +33302,9 @@ const octokit = new Octokit({
 
 const makeRequest = async () => {
   return await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
-    owner: 'jarrrule',
-    repo: 'actions-playground',
-    pull_number: '95',
+    owner: 'sainsburys-tech',
+    repo: 'argos-pdp-ui-service',
+    pull_number: '1719',
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
