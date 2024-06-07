04c5e0eff284315401f7708abd3c00aec565c7b8
jarrrule <sean.darley@sainsburys.co.uk>
2024-06-07 15:15:56 +0100

    FGH-7888: make request

--- .github/actions/getIssuesInRelease/index.js
+++ .github/actions/getIssuesInRelease/index.js
@@ -8,6 +8,7 @@ try {
   core.setOutput('jira-issues', ['NAP-1234', 'NAP-1235', 'NAP-1236', 'NAP-1237'])
   const token = core.getInput('token')
   console.log('can you see the token ', token)
+  console.log(JSON.stringify(makeRequest(), null, 2))
 } catch (error) {
   core.setFailed(error.message)
 }
@@ -16,11 +17,15 @@ const octokit = new Octokit({
   auth: core.getInput('token')
 })
 
-await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
-  owner: 'sainsburys-tech',
-  repo: 'argos-pdp-ui-service',
-  pull_number: '1719',
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
+const makeRequest = async () => {
+  await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
+    owner: 'sainsburys-tech',
+    repo: 'argos-pdp-ui-service',
+    pull_number: '1719',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+
