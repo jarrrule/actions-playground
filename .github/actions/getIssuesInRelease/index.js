b10725db32053539ae989988e5f4bb7171a891ff
jarrrule <sean.darley@sainsburys.co.uk>
2024-06-10 15:27:04 +0100

    NAP-3453: testing testing

--- .github/actions/getIssuesInRelease/index.js
+++ .github/actions/getIssuesInRelease/index.js
@@ -1,39 +1,40 @@
 import core from '@actions/core'
-import github from '@actions/github'
-import { Octokit } from "@octokit/core"
+import {Octokit} from "@octokit/core"
 
 const octokit = new Octokit({
   auth: core.getInput('token')
 })
 
-const makeRequest = async () => {
+const makeRequest = async (repoName, prNumber) => {
   return await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
     owner: 'sainsburys-tech',
-    repo: 'argos-pdp-ui-service',
-    pull_number: '1719',
+    repo: repoName,
+    pull_number: prNumber,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
   })
 }
 
+const filterJiraIssues = (commits) => {
+  return commits.data.filter(commit => {
+    const jiraIssues = commit.commit.message.match(/(NAP-\d+)/g)
+    if (jiraIssues) {
+      return true
+    } else {
+      const author = commit.author.author
+      console.log(`Commit by ${author} does not contain any JIRA issue`)
+      return false
+    }
+  }).map(filteredCommit => filteredCommit.commit.message.match(/(NAP-\d+)/g))[0]
+}
 
-
-
-(async () => {
-  try {
-    const prNumber = core.getInput('pr-number');
-    console.log(`Hello ${prNumber}!`);
-    core.setOutput('jira-issues', ['NAP-1234', 'NAP-1235', 'NAP-1236', 'NAP-1237'])
-    const token = core.getInput('token')
-    console.log('can you see the token ', token)
-    const response = await makeRequest()
-    console.log(JSON.stringify(response, null, 2))
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-})()
-
-
-
-
+try {
+  const prNumber = core.getInput('pr-number');
+  const repoName = core.getInput('repo-name')
+  const response = await makeRequest(repoName, prNumber)
+  const filteredIssues = filterJiraIssues(response)
+  core.setOutput('jira-issues', filteredIssues)
+} catch (error) {
+  core.setFailed(error.message)
+}
